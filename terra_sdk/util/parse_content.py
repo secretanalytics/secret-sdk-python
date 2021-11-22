bebbbd7459c324ddd698c93b52a129064368712d
Stephane Guilherme Gomes <stephane@linkaband.com>
2021-11-22 23:32:08 +0000

    Remove parse content for CommunityProposal

--- terra_sdk/util/parse_content.py
+++
@@ -1,9 +0,0 @@
-from terra_sdk.core.distribution.proposals import CommunityPoolSpendProposal
-
-from .base import create_demux
-
-parse_content = create_demux(
-    [
-        CommunityPoolSpendProposal,
-    ]
-)
